253d389784efdeb6a4341bd99aa0c57e5dab810e
jadhavajitt <jadhavajitt@gmail.com>
2023-02-16 23:20:23 +0530

    js_Assignment_01
    js_Assignment_01

---
+++ 01_variable_assign.js
@@ -0,0 +1,12 @@
+console.log("********step1*********");
+var myFullName; //variable Initialization
+myFullName = "Ajit Jadhav" //Variable Declaration -Name with fisrt & last Name
+console.log("My Full Name:", myFullName);
+myFullName = "Ajit Vishnu Jadhav" //Variable Declaration -Name updated with fisrt midddle & last Name
+console.log("My Full Name:",myFullName);
+console.log("*********step2**********");
+var myLove = "I Love only JS" //Variable initialization & Declaration
+console.log("My Love:", myLove);
+console.log("*********step3**********");
+var firstName="First Name: Ajit/", lastName="Last Name: Jadhav/", age = "Age: 31/", address = "Address: Mumbai/", pincode = "Pincode: 400084/", gender = "Gender: Male/", isMarried = "Married: Yes/"
+console.log(firstName,lastName,age,address,pincode,gender,isMarried)
